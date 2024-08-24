2c64633f6990d37eabd26dcd08e7297de22ec048
andresdotfelipe <andresdotfelipecs@outlook.com>
2024-08-23 20:28:00 -0500

    Fix CORS

--- src/app.js
+++ src/app.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const { appConfig } = require('./config');
-const api = require('./routes/api');
+const express = require("express");
+const cors = require("cors");
+const { appConfig } = require("./config");
+const api = require("./routes/api");
 
 const app = express();
 
 //  Settings
-app.set('port', appConfig.port);
+app.set("port", appConfig.port);
 
 //  Last element of whiteList is the front-end development origin.
 const whiteList = [
-    'https://coreinventory.netlify.app',
-    'http://coreinventory.netlify.app',
-    'http://localhost:3000'
+  "https://coreinventory.netlify.app",
+  "http://coreinventory.netlify.app",
 ];
-app.use(cors({
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      console.log("Origin:", origin);
+      if (whiteList.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    origin: (origin, callback) => {
-        if (whiteList.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //  Routes
-app.get('/', (req, res) => res.send('Welcome to Core Inventory API'));
-app.use('/api', api);
+app.get("/", (req, res) => res.send("Welcome to Core Inventory API"));
+app.use("/api", api);
 
 module.exports = app;
